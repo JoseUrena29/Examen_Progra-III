499ba6e43c72a1dcf7c58f3c25055bcb2a3c7a83
Jose Ureña Aguilar <urea.jose@hotmail.com>
2022-04-09 22:02:50 -0600

    Update Entity

--- Examen/Examen/SQLScripts/SQLQuery Insert-Destinos.sql
+++ Examen/Examen/SQLScripts/SQLQuery Insert-Destinos.sql
@@ -9,7 +9,7 @@ INSERT INTO [dbo].[Destinos]
 
 VALUES
 ('Hotel Papagayo Resort','Disfruta de tus vacaciones con todo incluido en Costa Rica en el fabuloso hotel sólo para adultos Occidental Papagayo - Adults only. ¡Reserva ahora!',
-'/Assets/images/HPR.jpg',56000),
+'/Assets/images/HPR1.jpg',56000),
 ('Holiday Inn San José','Sitio oficial de Holiday Inn San Jose-Aurola. Opiniones de clientes y reservas con nuestra garantía de mejor precio. Alojamiento y comida gratuitos para ...',
 '/Assets/images/HIS.jpg',49500),
 ('Puntarenas Hilton Resort','Este resort todo incluido frente al mar en Puntarenas ofrece gran área de playa de arena volcánica, varias piscinas al aire libre con terrazas solárium y diversos bares y restaurantes. ',
@@ -18,3 +18,4 @@ VALUES
 '/Assets/images/HCA.jpg',76100),
 ('Hotel Crocs Resort & Casino','Bienvenido a Crocs Resort & Casino, una fantástica opción para viajeros como tú. Las habitaciones de los huéspedes ofrecen servicios como televisor de pantalla plana, frigorífico y aire acondicionado, y los huéspedes pueden permanecer conectados con wifi gratuito. ',
 '/Assets/images/HCRC.jpg',145700)
+
