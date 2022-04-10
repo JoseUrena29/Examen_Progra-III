266b3615ba2ef1632e0c56eeaee5059afc6bd202
leandrovargasvalverde <leandro9700@gmail.com>
2022-04-09 21:47:32 -0600

    imagenes iguales

--- Examen/Examen/SQLScripts/SQLQuery Insert-Destinos.sql
+++ Examen/Examen/SQLScripts/SQLQuery Insert-Destinos.sql
@@ -6,33 +6,15 @@ INSERT INTO [dbo].[Destinos]
            ,[descripcion]
            ,[foto]
            ,[precio])
-     VALUES
-           ('Hotel Papagayo Resort'
-           ,'Disfruta de tus vacaciones con todo incluido en Costa Rica en el fabuloso hotel sólo para adultos Occidental Papagayo - Adults only. ¡Reserva ahora!'
-           ,'/Assets/images/HPR.jpg'
-           ,56000)
-GO
 
-INSERT INTO [dbo].[Destinos]
-           ([nombre]
-           ,[descripcion]
-           ,[foto]
-           ,[precio])
-     VALUES
-           ('Holiday Inn San José'
-           ,'Sitio oficial de Holiday Inn San Jose-Aurola. Opiniones de clientes y reservas con nuestra garantía de mejor precio. Alojamiento y comida gratuitos para ...'
-           ,'/Assets/images/HIS.jpg'
-           ,49500)
-GO
-
-INSERT INTO [dbo].[Destinos]
-           ([nombre]
-           ,[descripcion]
-           ,[foto]
-           ,[precio])
-     VALUES
-           ('Puntarenas Hilton Resort'
-           ,'Este resort todo incluido frente al mar en Puntarenas ofrece gran área de playa de arena volcánica, varias piscinas al aire libre con terrazas solárium y diversos bares y restaurantes. '
-           ,'/Assets/images/PHR.jpg'
-           ,52400)
-GO
+VALUES
+('Hotel Papagayo Resort','Disfruta de tus vacaciones con todo incluido en Costa Rica en el fabuloso hotel sólo para adultos Occidental Papagayo - Adults only. ¡Reserva ahora!',
+'/Assets/images/HPR.jpg',56000),
+('Holiday Inn San José','Sitio oficial de Holiday Inn San Jose-Aurola. Opiniones de clientes y reservas con nuestra garantía de mejor precio. Alojamiento y comida gratuitos para ...',
+'/Assets/images/HIS.jpg',49500),
+('Puntarenas Hilton Resort','Este resort todo incluido frente al mar en Puntarenas ofrece gran área de playa de arena volcánica, varias piscinas al aire libre con terrazas solárium y diversos bares y restaurantes. ',
+'/Assets/images/PHR.jpg',52400),
+('Hotel y Casino Amapola','El Hotel Amapola ofrece alojamiento en Jacó y cuenta con wifi gratis, bañera de hidromasaje y pileta al aire libre. El hotel cuenta con barbacoa, parque infantil y terraza, y los huéspedes pueden disfrutar de una comida en el restaurante. ',
+'/Assets/images/HCA.jpg',76100),
+('Hotel Crocs Resort & Casino','Bienvenido a Crocs Resort & Casino, una fantástica opción para viajeros como tú. Las habitaciones de los huéspedes ofrecen servicios como televisor de pantalla plana, frigorífico y aire acondicionado, y los huéspedes pueden permanecer conectados con wifi gratuito. ',
+'/Assets/images/HCRC.jpg',145700)
